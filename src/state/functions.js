3bf64c429b167535a019bdf75942d66540bf96cd
Mike Buonomo <mrb2590@gmail.com>
2019-01-16 19:20:53 -0500

    Process server errors

--- src/state/functions.js
+++ src/state/functions.js
@@ -1,9 +1,5 @@
 /**
  * Get an object from local storage.
- *
- * @param  {string} key  The key of the item to fetch.
- * @param  {string} type The storage type to use (local|session).
- * @return {object}      The object from storage.
  */
 export function getSavedState (key, type = 'local') {
   return JSON.parse(window[`${type}Storage`].getItem(key));
@@ -11,12 +7,22 @@ export function getSavedState (key, type = 'local') {
 
 /**
  * Save an object in local storage.
- *
- * @param  {string} key   The key of the item to fetch.
- * @param  {string} value The value to save in storage.
- * @param  {string} type  The storage type to use (local|session).
- * @return {void}
  */
 export function saveState (key, value, type = 'local') {
   window[`${type}Storage`].setItem(key, JSON.stringify(value));
+}
+
+/**
+ * Process errors from backend form validation.
+ */
+export function processInvalidForm (error, defaultMsg = 'There was an error.') {
+  if (error.response.status >= 500) return 'The was a server error.';
+  if (error.response.status !== 422) return defaultMsg;
+  let msg = '';
+  for (let errorType in error.response.data.errors) {
+    error.response.data.errors[errorType].forEach(errorMsg => {
+      msg += ` ${errorMsg}`;
+    });
+  }
+  return msg;
 }
